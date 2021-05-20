6f3b68c41f2c2daea4d530c137feb8169b19e328
Tatiana Campos <tccamposoliveira@gmail.com>
2021-05-19 21:50:45 -0300

    aula introdução ao javaScript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Tatiana Campos";
+var idade = 34;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert("Bem vindo(a) " + nome + " de " + idade + " anos! ");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+    // O replace troca uma palavra pela outra.
+console.log(frase.toUpperCase());
