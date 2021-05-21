6a5d600a208e01bb2c547931c6c6d7fd90cf9846
Tatiana Campos <tccamposoliveira@gmail.com>
2021-05-20 21:46:23 -0300

    aula array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
+var frutas = [{ nome: "maça", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+alert(fruta.cor);
+*/
+
+/*var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista.length);
+console.log(lista);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join("-"));
+*/
+
+/*
 var nome = "Tatiana Campos";
 var idade = 34;
 var idade2 = 10;
@@ -10,3 +33,4 @@ console.log(frase.replace("Japão", "Brasil"));
 //alert(frase.replace("Japão", "Brasil"));
     // O replace troca uma palavra pela outra.
 console.log(frase.toUpperCase());
+*/
