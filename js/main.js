b0397ca8f99951990c9a4e39ca8a0ff87d4de6ee
Tatiana Campos <tccamposoliveira@gmail.com>
2021-05-24 21:55:22 -0300

    aula condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
+/*
+var count;
+for (count = 0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count = count + 1;
+    //count++;
+    alert(count);
+}
+*/
+
+
+/*
+var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+/*
 var frutas = [{ nome: "maça", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
 console.log(frutas);
 alert(frutas[1].nome);
+*/
 
 
 /*
